7cd4efcbec0066876fcbe0fea1d5d7ac62a7e491
ethdm <ethan.hennequin--de_meyer@edu.univ-fcomte.fr>
2024-12-13 16:28:00 +0100

    lightbox, css, html, etc

--- src/js/script.js
+++ src/js/script.js
@@ -25,4 +25,19 @@ if (toggle && nav) {
     });
   }
 
-
+  const lb = document.querySelector("#lightbox");
+  const images = document.querySelectorAll("[data-full-img]");
+  
+  
+  images.forEach(image => {
+      image.addEventListener("click", function() {
+          const fullImg = image.dataset.fullImg;
+          const i = lb.querySelector("img");
+          i.src = fullImg;
+          lb.showModal();
+      });
+  });
+  
+  lb.addEventListener("click", function() {
+      lb.close();
+  });
