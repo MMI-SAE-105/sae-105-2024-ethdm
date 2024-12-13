346d0b8c92c09d509d28a7149f625998ac1b4485
ethdm <ethan.hennequin--de_meyer@edu.univ-fcomte.fr>
2024-12-13 10:03:50 +0100

    changements, modif html et css

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,3 @@
-const carouselContainer = document.querySelector('.carousel__container');
-const prevButton = document.querySelector('.carousel__button--prev');
-const nextButton = document.querySelector('.carousel__button--next');
-
-let currentIndex = 0;
-
-function updateCarousel() {
-    const offset = currentIndex * -100;
-    carouselContainer.style.transform = `translateX(${offset}%)`;
-}
-
-prevButton.addEventListener('click', () => {
-    currentIndex = (currentIndex > 0) ? currentIndex - 1 : 2; // Loop back to the last image
-    updateCarousel();
-});
-
-nextButton.addEventListener('click', () => {
-    currentIndex = (currentIndex < 2) ? currentIndex + 1 : 0; // Loop back to the first image
-    updateCarousel();
-});
 
 const toggle = document.querySelector(".menu-btn");
 const nav = document.querySelector(".menu");
@@ -44,3 +24,24 @@ if (toggle && nav) {
       }
     });
   }
+
+  const carouselContainer = document.querySelector('.carousel__container');
+const prevButton = document.querySelector('.carousel__button--prev');
+const nextButton = document.querySelector('.carousel__button--next');
+
+let currentIndex = 0;
+
+function updateCarousel() {
+    const offset = currentIndex * -100;
+    carouselContainer.style.transform = `translateX(${offset}%)`;
+}
+
+prevButton.addEventListener('click', () => {
+    currentIndex = (currentIndex > 0) ? currentIndex - 1 : 2; // Loop back to the last image
+    updateCarousel();
+});
+
+nextButton.addEventListener('click', () => {
+    currentIndex = (currentIndex < 2) ? currentIndex + 1 : 0; // Loop back to the first image
+    updateCarousel();
+});
